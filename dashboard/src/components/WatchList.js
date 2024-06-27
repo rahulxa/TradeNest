a79b2a43bb7952cc2709a77b753dd944b0226ac8
rahulxa <rahulac.5000@gmail.com>
2024-06-26 21:10:50 -0400

    setup watchlist component

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { watchlist } from '../data/data'
+import { Tooltip, Grow } from "@mui/material"
+
 
 function WatchList() {
   return (
-    <div>WatchList</div>
+    <div className='watchlist-container'>
+      <div className='search-container'>
+        <input
+          type='text'
+          name='search'
+          id='search'
+          placeholder='Search eg:infy, bse, nifty fut weekly, gold mcx'
+          className='search'
+        />
+        <span className='counts'>{watchlist.length}/50</span>
+      </div>
+
+      <ul className='list'>
+        {watchlist.map((stock, index) =>
+          <WatchListItem stock={stock} key={index} />)}
+      </ul>
+    </div>
   )
 }
 
