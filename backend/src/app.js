1c9edb145005f18648bc02188312d577abe79414
rahulxa <rahulac.5000@gmail.com>
2024-06-27 21:51:57 -0400

    complete holdings data fetch and setup cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 import express from "express"
+import bodyParser from "body-parser";
+import cors from "cors"
 
 import { holdingsRouter } from "./routes/allHoldings.routes.js";
 import { positionsRouter } from "./routes/allPositions.routes.js";
 
 const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.use("/api/v1/allHoldings", holdingsRouter);
 app.use("/api/v1/allPositions", positionsRouter);
--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,46 +1,59 @@
-import React from 'react'
-import { holdings } from "../data/data"
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 
 function Holdings() {
+
+  const [allHoldings, setAllHoldings] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/api/v1/allHoldings")
+      .then((res) => {
+        setAllHoldings(res.data)
+        console.log("data:", res.data);
+      })
+  }, []);
+
+
   return (
     <>
-      <h3 className='title'>Holdings ({holdings.length})</h3>
+      <h3 className='title'>Holdings ({allHoldings.length})</h3>
       <div className='order-table'>
         <table>
-          <tr>
-            <th>Instrument</th>
-            <th>Qty.</th>
-            <th>Avg. cost</th>
-            <th>LTP</th>
-            <th>Cur. val</th>
-            <th>P&L</th>
-            <th>Net chg.</th>
-            <th>Day. chg</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Instrument</th>
+              <th>Qty.</th>
+              <th>Avg. cost</th>
+              <th>LTP</th>
+              <th>Cur. val</th>
+              <th>P&L</th>
+              <th>Net chg.</th>
+              <th>Day. chg</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allHoldings.map((stock, index) => {
+              const currValue = stock.price * stock.qty;
+              const isProfit = currValue - stock.avg * stock.qty >= 0.0;
+              const profClass = isProfit ? "profit" : "loss";
+              const dayClass = stock.isLoss ? "loss" : "profit"
 
-          {holdings.map((stock, index) => {
-            const currValue = stock.price * stock.qty;
-            const isProfit = currValue - stock.avg * stock.qty >= 0.0;
-            const profClass = isProfit ? "profit" : "loss";
-            const dayClass = stock.isLoss ? "loss" : "profit"
-
-            return (
-              <tr key={index}>
-                <td>{stock.name}</td>
-                <td>{stock.qty}</td>
-                <td>{stock.avg.toFixed(2)}</td>
-                <td>{stock.price.toFixed(2)}</td>
-                <td>{currValue.toFixed(2)}</td>
-                <td className={profClass}>{(currValue - (stock.avg * stock.qty)).toFixed(2)}</td>
-                <td className={profClass}>{stock.net}</td>
-                <td className={dayClass}>{stock.day}</td>
-              </tr>
-            )
-          })}
-
+              return (
+                <tr key={index}>
+                  <td>{stock.name}</td>
+                  <td>{stock.qty}</td>
+                  <td>{stock.avg.toFixed(2)}</td>
+                  <td>{stock.price.toFixed(2)}</td>
+                  <td>{currValue.toFixed(2)}</td>
+                  <td className={profClass}>{(currValue - (stock.avg * stock.qty)).toFixed(2)}</td>
+                  <td className={profClass}>{stock.net}</td>
+                  <td className={dayClass}>{stock.day}</td>
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
       </div>
-
       <div className="row">
         <div className="col">
           <h5>
