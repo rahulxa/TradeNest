9fe38eabb7eb17afab6ddbc3352b99816028bed9
rahulxa <rahulac.5000@gmail.com>
2024-06-28 21:18:09 -0400

    add summary component

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+
 
 function Orders() {
   return (
-    <div>s</div>
+    <div className="orders">
+      <div className="no-orders">
+        <p>You haven't placed any orders today</p>
+
+        <Link to={"/"} className="btn">
+          Get started
+        </Link>
+      </div>
+    </div>
   )
 }
 
